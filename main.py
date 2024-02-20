9ea2cc0c91c0fac989355737319deb4b59a0b1b2
Adam <144299219+adamsaleh1112@users.noreply.github.com>
2024-02-20 18:59:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -25,12 +25,14 @@ while (True):
     maskimg = cv2.bitwise_and(edges, edges, mask=mask)
 
     # DETECT LINES
-    lines = cv2.HoughLinesP(maskimg, rho=1, theta=np.pi / 180, threshold=10, minLineLength=5, maxLineGap=10)
+    lines = cv2.HoughLinesP(maskimg, rho=1, theta=np.pi / 180, threshold=5, minLineLength=15, maxLineGap=20)
+
 
     # IF LINES NOT NONE
     if lines is not None:
 
         midlines = []  # List to store midlines between lines
+
 
         # FOR LINE IN LINES
         for line1 in lines:
@@ -42,6 +44,7 @@ while (True):
                 slope_1 = (y2_1 - y1_1) / (x2_1 - x1_1)
 
             cv2.line(img, (x1_1, y1_1), (x2_1, y2_1), (255, 0, 0), 6)
+
 
             for line2 in lines:
                 x1_2, y1_2, x2_2, y2_2 = line2[0]
@@ -63,6 +66,7 @@ while (True):
 
                     midlines.append(((mid_x1, mid_y1), (mid_x2, mid_y2)))
 
+
         # DRAW MIDLINES
         for midline in midlines:
             cv2.line(img, midline[0], midline[1], (255, 0, 0), 4)
@@ -71,6 +75,7 @@ while (True):
         cv2.rectangle(img, (x, y), (x + w, y + h), (255, 255, 255), 2)
 
         cv2.imshow('frame', img)
+
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
